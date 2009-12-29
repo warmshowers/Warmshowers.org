e305bee77fa9ed1c0ef204a32ccb498ab0f9f16f
rfay <randy@randyfay.com>
2009-12-29 00:14:10 -0700

    good, working version. Not half bad

--- sites/all/modules/contrib/translatablecomments/translatablecomments.js
+++ sites/all/modules/contrib/translatablecomments/translatablecomments.js
@@ -1,12 +1,16 @@
 //$Id:  $
 /**
+ * @file
+ * Javascript to support translatable regions on the page.
+ * 
+ * Primarily uses the jquery.translate plugin.
  * Adapted from translatablecomments module by Dave Trainer.
  */
 Drupal.behaviors.translatablecomments = function (context) {
   var link = $("<a />").attr("href", "#").attr("class","translator");
   var wrapper = $("<span></span>").attr("class","translator");
   var languages = ["it", "ja", "nl", "pt", "fr", "de", "es", "en"];
-  var translate_classes = Drupal.settings.translatablecomments.translate_classes;
+  var translate_selectors = Drupal.settings.translatablecomments.translate_selectors;
   var auto_translate = Drupal.settings.translatablecomments.auto_translate;
   var my_language_code = Drupal.settings.translatablecomments.my_language_code;
   var my_language_name = Drupal.settings.translatablecomments.my_language_name;
@@ -25,7 +29,7 @@ Drupal.behaviors.translatablecomments = function (context) {
   
   // If auto_translate, we'll just translate the block no matter what.
   if (auto_translate) {
-    $(translate_classes).each(function (i) {
+    $(translate_selectors).each(function (i) {
       $(this).translate(my_language_code);
       $.translate.getBranding().appendTo($(this)).prepend(Drupal.t("Translation") + " ");
     });
@@ -41,79 +45,32 @@ Drupal.behaviors.translatablecomments = function (context) {
           break;
         }
       }
-      $(translate_classes).each(function (i) {
+      $(translate_selectors).each(function (i) {
         var element = $(this);
         button = $("<input type='submit' value='" + Drupal.t('Translate To') + "' />")
         .click(function(){
-          element.translate($(this).parent().children('select').val(), { 
-            not: '.option, #demo, #source, pre, .jq-translate-ui', //exclude these elements
-            fromOriginal:true //always translate from orig (even after the page has been translated)
-          })
+          translate_element(element, $(this).parent().children('select').val());
         });
         $.translate.ui('select', 'option')
         .change(function(){ //when selecting another language
-          element.translate($(this).val(), { 
-            not: '.option, #demo, #source, pre, .jq-translate-ui', //exclude these elements
-            fromOriginal:true //always translate from orig (even after the page has been translated)
-          })
+          translate_element(element, $(this).val());
         })
         .val(browser_language_name)
         .prependTo($(this))
         .parent()
         .prepend(button);
         
-        $.translate.getBranding().appendTo($(this)).prepend(Drupal.t("Translation") + " ");
       });
     });
   }
 }
 
-
-/*
-Drupal.behaviors.translatablecomments = function (context) {
-  var link = $("<a />").attr("href", "#").attr("class","translator");
-  var wrapper = $("<span></span>").attr("class","translator");
-  var languages = ["it", "ja", "nl", "pt", "fr", "de", "es", "en"];
-  var translate_classes = Drupal.settings.translatablecomments.translate_classes;
-  var auto_translate = Drupal.settings.translatablecomments.auto_translate;
-  var my_language = Drupal.settings.translatablecomments.my_language;
-
-  $(translate_classes).each(function (i) {
-    if (auto_translate) {
-      // detect the language
-      // Get the current language
-      // translate/replace
-      var text = $(this).html().substr(0,4999);
-      var div = $(this);
-      google.language.detect(text, function(result) {
-        if (!result.error && result.language && my_language != result.language) {
-          google.language.translate(text, result.language, my_language, function(result) {
-            if (!result.error) {
-              div.html(result.translation);
-            } else {
-              alert(result.error.message);
-            }
-          });
-        }
-      });
-    }
-    else {
-      var w = $(wrapper).clone().insertBefore(this);
-      jQuery.each(languages, function(x){
-        w.append(link.clone().html(languages[x]).click(function () {
-          var n = $(this).parent().next();
-          google.language.translate(n.html(), "", languages[x], function(result) {
-            if (!result.error) {
-              n.html(result.translation);
-            } else {
-              alert(result.error.message);
-            }
-          });
-          this.blur();
-          return false;
-        }));
-      });
-    }
+function translate_element(element, target_language) {
+  element.translate(target_language, { 
+    //TODO: Make user-configurable exclusions
+    not: '.option, #demo, #source, pre, .jq-translate-ui', //exclude these elements
+    fromOriginal:true //always translate from orig (even after the page has been translated)
   });
+  element.children('div.gBranding').remove();
+  $.translate.getBranding().appendTo(element).prepend(Drupal.t("Translation") + " ");
 }
-*/
