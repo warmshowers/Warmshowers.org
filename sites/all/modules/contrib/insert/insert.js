5a3071454f0bb06a11c9e6a66375b8e690d81845
rfay <randy@randyfay.com>
2010-01-20 09:45:19 -0700

    Added jquery_update, insert, mysqlreport, image_resize_filter

---
+++ sites/all/modules/contrib/insert/insert.js
@@ -0,0 +1,115 @@
+// $Id: insert.js,v 1.4 2009/10/22 00:12:25 quicksketch Exp $
+
+/**
+ * Behavior to add "Insert" buttons.
+ */
+Drupal.behaviors.insert = function(context) {
+  if (typeof(insertTextarea) == 'undefined') {
+    insertTextarea = $('#edit-body').get(0) || false;
+  }
+
+  // Keep track of the last active textarea (if not using WYSIWYG).
+  $('.node-form textarea:not([name$="[data][title]"])', context).focus(insertSetActive).blur(insertRemoveActive);
+
+  // Add the click handler to the insert button.
+  $('.insert-button', context).click(insert);
+
+  function insertSetActive() {
+    insertTextarea = this;
+    this.insertHasFocus = true;
+  }
+
+  function insertRemoveActive() {
+    if (insertTextarea == this) {
+      var thisTextarea = this;
+      setTimeout(function() {
+        thisTextarea.insertHasFocus = false;
+      }, 1000);
+    }
+  }
+
+  function insert() {
+    var widgetType = $(this).attr('rel');
+    var settings = Drupal.settings.insert.widgets[widgetType];
+    var wrapper = $(this).parents(settings.wrapper).filter(':first').get(0);
+    var style = $('.insert-style', wrapper).val();
+    var content = $('input.insert-template[name$="[' + style + ']"]', wrapper).val();
+
+    // Update replacements.
+    for (var fieldName in settings.fields) {
+      var fieldValue = $(settings.fields[fieldName], wrapper).val();
+      var fieldRegExp = new RegExp('__' + fieldName + '__', 'g');
+      if (fieldValue) {
+        content = content.replace(fieldRegExp, fieldValue);
+      }
+    }
+    // Cleanup unused replacements.
+    content = content.replace(/__([a-z0-9_]+)__/g, '');
+
+    // Check for a maximum dimension and scale down the width if necessary.
+    // This is intended for use with Image Resize Filter.
+    var widthMatches = content.match(/width[ ]*=[ ]*"(\d*)"/i);
+    var heightMatches = content.match(/height[ ]*=[ ]*"(\d*)"/i);
+    if (settings.maxWidth && widthMatches && parseInt(widthMatches[1]) > settings.maxWidth) {
+      var insertRatio = settings.maxWidth / widthMatches[1];
+      var width = settings.maxWidth;
+      content = content.replace(/width[ ]*=[ ]*"?(\d*)"?/i, 'width="' + width + '"');
+
+      if (heightMatches) {
+        var height = Math.round(heightMatches[1] * insertRatio);
+        content = content.replace(/height[ ]*=[ ]*"?(\d*)"?/i, 'height="' + height + '"');
+      }
+    }
+
+    // Always work in normal text areas that currently have focus.
+    if (insertTextarea && insertTextarea.insertHasFocus) {
+      insertAtCursor(insertTextarea, content);
+    }
+    // Direct tinyMCE support.
+    else if (typeof(tinyMCE) != 'undefined' && tinyMCE.activeEditor) {
+      tinyMCE.activeEditor.execCommand('mceInsertContent', false, content);
+    }
+    // WYSIWYG support, should work in all editors if available.
+    else if (Drupal.wysiwyg && Drupal.wysiwyg.activeId) {
+      Drupal.wysiwyg.instances[Drupal.wysiwyg.activeId].insert(content)
+    }
+    // FCKeditor module support.
+    else if (typeof(FCKeditorAPI) != 'undefined' && fckActiveId) {
+      FCKeditorAPI.Instances[fckActiveId].InsertHtml(content);
+    }
+    // Direct FCKeditor support (only body field supported).
+    else if (typeof(FCKeditorAPI) != 'undefined' && FCKeditorAPI.Instances['edit-body']) {
+      FCKeditorAPI.Instances['edit-body'].InsertHtml(content);
+    }
+    // Direct CKeditor support (only body field supported).
+    else if (typeof(CKEDITOR) != 'undefined' &&  CKEDITOR.instances['edit-body']) {
+      CKEDITOR.instances['edit-body'].insertHtml(content);
+    }
+    else if (insertTextarea) {
+      insertAtCursor(insertTextarea, content);
+    }
+
+    return false;
+  }
+
+  function insertAtCursor(editor, content) {
+    // IE support.
+    if (document.selection) {
+      editor.focus();
+      sel = document.selection.createRange();
+      sel.text = content;
+    }
+
+    // Mozilla/Firefox/Netscape 7+ support.
+    else if (editor.selectionStart || editor.selectionStart == '0') {
+      var startPos = editor.selectionStart;
+      var endPos = editor.selectionEnd;
+      editor.value = editor.value.substring(0, startPos)+ content + editor.value.substring(endPos, editor.value.length);
+    }
+
+    // Fallback, just add to the end of the content.
+    else {
+      editor.value += content;
+    }
+  }
+}
