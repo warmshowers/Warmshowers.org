7cd6af14530862190c0ce71eb62953137c07b701
rfay <randy@randyfay.com>
2010-03-05 22:52:37 -0700

    newest translatable regions, from dev

--- sites/all/modules/contrib/translatableregions/README.txt
+++ sites/all/modules/contrib/translatableregions/README.txt
@@ -1,4 +1,4 @@
-// $Id: README.txt,v 1.1 2008/04/15 21:23:05 davetrainer Exp $
+// $Id: README.txt,v 1.2 2010/02/23 19:52:53 rfay Exp $
 
 translatableregions adds the ability for the user to machine-translate
 specific regions which you specify as CSS selectors.
@@ -6,13 +6,16 @@ specific regions which you specify as CSS selectors.
 Installation
 ------------
 
+The 2.x-dev version of the jquery_update module is currently required for
+IE8 support. Get it at http://drupal.org/project/jquery_update.
+
 Copy the translatableregions folder to your sites/all/modules directory, 
 then enable it at Administer > Site Building > Modules.
 
-Configure the regions that you want to work with at 
-Administer->Site Configuration->Translatable Regions
+Configure the CSS selectors for the regions that you want to work with at 
+Administer->Site Configuration->Translatable Regions.
 
 Author
 ------
-Dave Trainer
-dave AT minimalmedia DOT com
+Randy Fay
+randy AT randyfay.com
--- sites/all/modules/contrib/translatableregions/translatableregions.js
+++ sites/all/modules/contrib/translatableregions/translatableregions.js
@@ -1,72 +1,73 @@
-//$Id:  $
+//$Id: translatableregions.js,v 1.3.2.1 2010/02/24 21:37:57 rfay Exp $
 /**
  * @file
  * Javascript to support translatable regions on the page.
  * 
- * Primarily uses the jquery.translate plugin.
+ * Uses the jquery.translate plugin.
  * Adapted from translatableregions module by Dave Trainer.
  */
 Drupal.behaviors.translatableregions = function (context) {
-  var link = $("<a />").attr("href", "#").attr("class","translator");
-  var wrapper = $("<span></span>").attr("class","translator");
-  var languages = ["it", "ja", "nl", "pt", "fr", "de", "es", "en"];
-  var translate_selectors = Drupal.settings.translatableregions.translate_selectors;
-  var auto_translate = Drupal.settings.translatableregions.auto_translate;
-  var my_language_code = Drupal.settings.translatableregions.my_language_code;
-  var my_language_name = Drupal.settings.translatableregions.my_language_name;
-  var browser_language;
-  if (navigator.userLanguage) // Explorer
-    browser_language = navigator.userLanguage;
-  else if (navigator.language) {
-    browser_language = navigator.language;
-  } 
-  else {
-    browser_language = "en";
-  }
-  browser_language = browser_language.substr(0,2);
-  browser_language_name = "English";
-  
-  
-  // If auto_translate, we'll just translate the block no matter what.
-  if (auto_translate) {
-    $(translate_selectors).each(function (i) {
-      // TODO: Handle the mapping of browser language to Google's languages. 
-      // They aren't all the same. Portuguese is the biggest example.
-      $(this).translate(browser_language);
-      $.translate.getBranding().appendTo($(this)).prepend(Drupal.t("Translation") + " ");
-    });
-  } 
-  // Otherwise, put a select at the top of the block offering translation.
-  else {
-    $.translate(function(){ //when the Google Language API is loaded
-      var languages = $.translate.getLanguages(true);
-      for (var key in languages) {
-        if (languages[key].substr(0,2) == browser_language) {
-          // Capitalize so that it will match the name in the select.
-          browser_language_name = key.charAt(0)+key.substring(1).toLowerCase();
-          break;
-        }
-      }
-      $(translate_selectors).each(function (i) {
-        var element = $(this);
-        button = $("<input type='submit' value='" + Drupal.t('Translate To') + "' />")
-        .click(function(){
-          translate_element(element, $(this).parent().children('select').val());
+  $.translate(function() {  // After translation engine is ready.
+
+    var link = $("<a />").attr("href", "#").attr("class","translator");
+    var wrapper = $("<span></span>").attr("class","translator");
+    var translate_selectors = Drupal.settings.translatableregions.translate_selectors;
+    var auto_translate = Drupal.settings.translatableregions.auto_translate;  
+    var hide_translate_button = Drupal.settings.translatableregions.hide_translate_button;
+    var always_show_translate_buttons = Drupal.settings.translatableregions.always_show_translate_buttons;
+
+    // If auto_translate, we'll just translate the block no matter what.
+    if (auto_translate) {
+      $.translate(function(){ //when the Google Language API is loaded
+        var browser_language = get_browser_language();
+        $(translate_selectors).each(function (i) {
+          var element = $(this);
+          translate_element(element, browser_language);
         });
-        $.translate.ui('select', 'option')
-        .change(function(){ //when selecting another language
-          translate_element(element, $(this).val());
-        })
-        .val(browser_language_name)
-        .prependTo($(this))
-        .parent()
-        .prepend(button);
-        
       });
-    });
-  }
+
+    } 
+    // Otherwise, put a select at the top of the block offering translation.
+    else if (always_show_translate_buttons || !hide_translate_button) {
+      $.translate(function(){ //when the Google Language API is loaded
+        var browser_language = get_browser_language();
+        $(translate_selectors).each(function (i) {
+          var element = $(this);
+          var button = $("<input type='submit' />")
+          .val(Drupal.t("Translate to"))
+          .click(function(){
+            translate_element(element, $(this).parent().children('select').val());
+          });
+
+          // Maintainer provided wonderful help on this at
+          // http://code.google.com/p/jquery-translate/issues/detail?id=30
+          $.translate.ui({
+            tags: ["select", "option"],
+            filter: $.translate.isTranslatable,
+            label: $.translate.toNativeLanguage, //default
+            includeUnknown: false
+          })
+          .val(browser_language)
+          .change(function(){ //when selecting another language
+            translate_element(element, $(this).val());
+          })
+          .val(browser_language)
+          .prependTo($(this))
+          .parent()
+          .prepend(button);
+
+        });
+      });
+    };
+  });
 }
 
+/**
+ * Translate an element into a target language.
+ * @param element
+ * @param target_language
+ *   The language code to translate to. Must match one of Google's array.
+ */
 function translate_element(element, target_language) {
   element.translate(target_language, { 
     //TODO: Make user-configurable exclusions
@@ -74,5 +75,14 @@ function translate_element(element, target_language) {
     fromOriginal:true //always translate from orig (even after the page has been translated)
   });
   element.children('div.gBranding').remove();
-  $.translate.getBranding().appendTo(element).prepend(Drupal.t("Translation") + " ");
+  $.translate.getBranding().appendTo(element).prepend(Drupal.t("Automatic translation") + " ");
+}
+
+/**
+ * Determine the browser language and then match it to one of Google's languages.
+ */
+function get_browser_language() {
+  var lang = navigator.language || navigator.browserLanguage || navigator.systemLanguage || navigator.userLanguage;
+  var langcode = $.translate.toLanguageCode(lang) || lang.substr(0,2);
+  return langcode;
 }
