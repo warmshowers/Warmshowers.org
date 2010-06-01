f685ff0958e3431133e29064d20773754e3b7dc6
rfay <randy@randyfay.com>
2010-06-01 15:26:40 -0600

    updated jquery_translate

--- sites/all/modules/contrib/translatableregions/jquery.translate/jquery.translate-NCT,core,NCT-adapter,ui,progress,native.js
+++ sites/all/modules/contrib/translatableregions/jquery.translate/jquery.translate-NCT,core,NCT-adapter,ui,progress,native.js
@@ -916,3 +916,77 @@ $.translate.ui.defaults = $.extend({}, defaults);
 	
 	return progress;
 };
+
+/*!-
+ * Native language names extension for the jQuery Translate plugin 
+ * Version: 1.4.5
+ * http://code.google.com/p/jquery-translate/
+ */
+;(function($){
+$.translate.extend({
+
+	toNativeLanguage: function(lang){ 
+		return $.translate.nativeLanguages[ lang ] || 
+			$.translate.nativeLanguages[ $.translate.toLanguageCode(lang) ];
+	},
+	
+	nativeLanguages: {
+		"af":"Afrikaans",
+		"be":"Беларуская",
+		"is":"Íslenska",
+		"ga":"Gaeilge",
+		"mk":"Македонски",
+		"ms":"Bahasa Melayu",
+		"sw":"Kiswahili",
+		"cy":"Cymraeg",
+		"yi":"ייִדיש",
+		
+		"sq":"Shqipe",
+		"ar":"العربية",
+		"bg":"Български",
+		"ca":"Català",
+		"zh":"中文",
+		"zh-CN":"简体中文",
+		"zh-TW":"繁體中文",
+		"hr":"Hrvatski",
+		"cs":"Čeština",
+		"da":"Dansk",
+		"nl":"Nederlands",
+		"en":"English",
+		"et":"Eesti",
+		"tl":"Tagalog",
+		"fi":"Suomi",
+		"fr":"Français",
+		"gl":"Galego",
+		"de":"Deutsch",
+		"el":"Ελληνικά",
+		"iw":"עברית",
+		"hi":"हिन्दी",
+		"hu":"Magyar",
+		"id":"Bahasa Indonesia",
+		"it":"Italiano",
+		"ja":"日本語",
+		"ko":"한국어",
+		"lv":"Latviešu",
+		"lt":"Lietuvių",
+		"mt":"Malti",
+		"no":"Norsk",
+		"fa":"فارسی",
+		"pl":"Polski",
+		"pt-PT":"Português",
+		"ro":"Român",
+		"ru":"Русский",
+		"sr":"Српски",
+		"sk":"Slovenský",
+		"sl":"Slovenski",
+		"es":"Español",
+		"sv":"Svenska",
+		"th":"ไทย",
+		"tr":"Türkçe",
+		"uk":"Українська",
+		"vi":"Tiếng Việt"
+	}
+
+});
+
+})(jQuery);
--- sites/all/modules/contrib/translatableregions/translatableregions.js
+++ sites/all/modules/contrib/translatableregions/translatableregions.js
@@ -1,52 +1,65 @@
-//$Id: translatableregions.js,v 1.3 2010/02/24 00:19:05 rfay Exp $
+//$Id: translatableregions.js,v 1.3.2.1 2010/02/24 21:37:57 rfay Exp $
 /**
  * @file
  * Javascript to support translatable regions on the page.
  * 
- * Primarily uses the jquery.translate plugin.
+ * Uses the jquery.translate plugin.
  * Adapted from translatableregions module by Dave Trainer.
  */
 Drupal.behaviors.translatableregions = function (context) {
-  var link = $("<a />").attr("href", "#").attr("class","translator");
-  var wrapper = $("<span></span>").attr("class","translator");
-  var translate_selectors = Drupal.settings.translatableregions.translate_selectors;
-  var auto_translate = Drupal.settings.translatableregions.auto_translate;  
-  var hide_translate_button = Drupal.settings.translatableregions.hide_translate_button;
-  var always_show_translate_buttons = Drupal.settings.translatableregions.always_show_translate_buttons;
-  
-  // If auto_translate, we'll just translate the block no matter what.
-  if (auto_translate) {
-    $.translate(function(){ //when the Google Language API is loaded
-      var browser_language = get_browser_language();
-      $(translate_selectors).each(function (i) {
-        var element = $(this);
-        translate_element(element, browser_language);
-      });
-    });
+  $.translate(function() {  // After translation engine is ready.
 
-  } 
-  // Otherwise, put a select at the top of the block offering translation.
-  else if (always_show_translate_buttons || !hide_translate_button) {
-    $.translate(function(){ //when the Google Language API is loaded
-      var browser_language = get_browser_language();
-      $(translate_selectors).each(function (i) {
-        var element = $(this);
-        button = $("<input type='submit' value='" + Drupal.t('Translate To') + "' />")
-        .click(function(){
-          translate_element(element, $(this).parent().children('select').val());
+    var link = $("<a />").attr("href", "#").attr("class","translator");
+    var wrapper = $("<span></span>").attr("class","translator");
+    var translate_selectors = Drupal.settings.translatableregions.translate_selectors;
+    var auto_translate = Drupal.settings.translatableregions.auto_translate;  
+    var hide_translate_button = Drupal.settings.translatableregions.hide_translate_button;
+    var always_show_translate_buttons = Drupal.settings.translatableregions.always_show_translate_buttons;
+
+    // If auto_translate, we'll just translate the block no matter what.
+    if (auto_translate) {
+      $.translate(function(){ //when the Google Language API is loaded
+        var browser_language = get_browser_language();
+        $(translate_selectors).each(function (i) {
+          var element = $(this);
+          translate_element(element, browser_language);
         });
-        $.translate.ui('select', 'option')
-        .change(function(){ //when selecting another language
-          translate_element(element, $(this).val());
-        })
-        .val(browser_language)
-        .prependTo($(this))
-        .parent()
-        .prepend(button);
-        
       });
-    });
-  }
+
+    } 
+    // Otherwise, put a select at the top of the block offering translation.
+    else if (always_show_translate_buttons || !hide_translate_button) {
+      $.translate(function(){ //when the Google Language API is loaded
+        var browser_language = get_browser_language();
+        $(translate_selectors).each(function (i) {
+          var element = $(this);
+          var button = $("<input type='submit' />")
+          .val(Drupal.t("Translate to"))
+          .click(function(){
+            translate_element(element, $(this).parent().children('select').val());
+          });
+
+          // Maintainer provided wonderful help on this at
+          // http://code.google.com/p/jquery-translate/issues/detail?id=30
+          $.translate.ui({
+            tags: ["select", "option"],
+            filter: $.translate.isTranslatable,
+            label: $.translate.toNativeLanguage, //default
+            includeUnknown: false
+          })
+          .val(browser_language)
+          .change(function(){ //when selecting another language
+            translate_element(element, $(this).val());
+          })
+          .val(browser_language)
+          .prependTo($(this))
+          .parent()
+          .prepend(button);
+
+        });
+      });
+    };
+  });
 }
 
 /**
@@ -69,37 +82,7 @@ function translate_element(element, target_language) {
  * Determine the browser language and then match it to one of Google's languages.
  */
 function get_browser_language() {
-  var browser_language;
-  
-  // First, find the language the browser reports to us. Probably
-  // something like en-US.
-  if (navigator.userLanguage) // Explorer
-    browser_language = navigator.userLanguage;
-  else if (navigator.language) {
-    browser_language = navigator.language;
-  } 
-  else {
-    browser_language = "en";
-  }
-
-  // Now get Google's list of languages and see if we can match it up.
-  var languages = $.translate.getLanguages(true);
-  if (!(browser_language in languages)) {
-    // Try it without the country code
-    if (browser_language.substr(0,2) in languages) {
-      browser_language = browser_language.substr(0,2);
-    } 
-    // Otherwise we have to look through the whole list looking for a match
-    // on the language portion!
-    else {
-      browser_language = browser_language.substr(0,2);
-      for (var langcode in languages) {
-        if (languages[langcode].substr(0,2) == browser_language) {
-          browser_language = languages[langcode];
-          break;
-        }
-      } 
-    }
-  }
-  return browser_language;
+  var lang = navigator.language || navigator.browserLanguage || navigator.systemLanguage || navigator.userLanguage;
+  var langcode = $.translate.toLanguageCode(lang) || lang.substr(0,2);
+  return langcode;
 }
