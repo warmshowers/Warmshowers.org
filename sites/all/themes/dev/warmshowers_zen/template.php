83b0c888e5445581ea9dea58dd3b272e97242d7f
Randy Fay <randy@randyfay.com>
2012-05-16 12:19:18 -0600

    Change theme to put description before form element

--- sites/all/themes/dev/warmshowers_zen/template.php
+++ sites/all/themes/dev/warmshowers_zen/template.php
@@ -113,6 +113,43 @@ function warmshowers_zen_status_messages($display = NULL) {
   }
   return $output;
 }
+
+/**
+ * Replace theme('form_element') to put the description ahead of the form element.
+ *
+ * @param $element
+ * @param $value
+ * @return string
+ */
+function warmshowers_zen_form_element($element, $value) {
+  // This is also used in the installer, pre-database setup.
+  $t = get_t();
+
+  $output = '<div class="form-item"';
+  if (!empty($element['#id'])) {
+    $output .= ' id="' . $element['#id'] . '-wrapper"';
+  }
+  $output .= ">\n";
+  $required = !empty($element['#required']) ? '<span class="form-required" title="' . $t('This field is required.') . '">*</span>' : '';
+
+  if (!empty($element['#title'])) {
+    $title = $element['#title'];
+    if (!empty($element['#id'])) {
+      $output .= ' <label for="' . $element['#id'] . '">' . $t('!title: !required', array('!title' => filter_xss_admin($title), '!required' => $required)) . "</label>\n";
+    }
+    else {
+      $output .= ' <label>' . $t('!title: !required', array('!title' => filter_xss_admin($title), '!required' => $required)) . "</label>\n";
+    }
+  }
+  if (!empty($element['#description'])) {
+    $output .= ' <div class="description">' . $element['#description'] . "</div>\n";
+  }
+  $output .= " $value\n";
+  $output .= "</div>\n";
+
+  return $output;
+}
+
 /**
  * Override or insert variables into all templates.
  *
