f1aebe2185e107d2c72eed433c6559249e289e4d
Chris Russo <chris.andrews.russo@gmail.com>
2012-05-30 19:37:44 -0400

    Change expansion js to operate off separate links

--- sites/all/modules/dev/wsmap/js/expand_map.js
+++ sites/all/modules/dev/wsmap/js/expand_map.js
@@ -5,53 +5,57 @@ Drupal.behaviors.expandMap = function(context) {
     var originalMapBlockHeight = $('div.block-wsmap').height();
 
     $("#expand_map").click(function(){
-        if($(this).html() == 'Expand Map'){
-            $(".region-sidebar-first .section").hide(1000);
-            $(".region-highlight").hide(1000);
-            $("body.with-highlight #navigation .section").hide(1000);
-            $(".sidebar-first #content").animate({
-                marginLeft: "0px",
-                width: "100%"
-            }, 1000, function(){
-                $("#expand_map").html("Collapse Map");
-                $("body.with-highlight #navigation .section").css("top","-69px");
-                $("body.with-highlight #navigation .section").show("fast");
-            });
-            // If the window can handle, let's expand the height too
-            if($(window).height() - 150 > $('#wsmap_map').height()){
-                $("#wsmap_map").animate({
-                    'height': $(window).height()  - 150 + 'px'
-                }, 1000);
-                $("div.block-wsmap").animate({
-                    'height': $(window).height()  - 130 + 'px'
-                }, 1000);
-            }
+        $(".region-sidebar-first .section").hide(1000);
+        $(".region-highlight").hide(1000);
+        $("body.with-highlight #navigation .section").hide(1000);
+        $(".sidebar-first #content").animate({
+            marginLeft: "0px",
+            width: "100%"
+        }, 1000, function(){
+            $("#collapse_map").show();
+            $("body.with-highlight #navigation .section").css("top","-69px");
+            $("body.with-highlight #navigation .section").show("fast");
+        });
+        // If the window can handle, let's expand the height too
+        if($(window).height() - 150 > $('#wsmap_map').height()){
+            $("#wsmap_map").animate({
+                'height': $(window).height()  - 150 + 'px'
+            }, 1000);
+
+            $("#content .block-wsmap").animate({
+                'height': $(window).height()  - 130 + 'px'
+            }, 1000);
 
         }
-        else {
-            $("body.with-highlight #navigation .section").hide(1000);
-            $(".region-sidebar-first .section").show(1000);
-            $(".region-highlight").show(1000);
-            $(".sidebar-first #content").animate({
-                marginLeft: '240px',
-                width: '720px'
-            }, 1000, function(){
-                $("#expand_map").html("Expand Map");
-                $("body.with-highlight #navigation .section").css("top","-260px");
-                $("body.with-highlight #navigation .section").show("fast");
-            });
 
-            // If the height was expanded on expansion, let's collapse to original height
-            if($('#wsmap_map').height() > originalMapHeight){
-                $("#wsmap_map").animate({
-                    'height':originalMapHeight + 'px'
-                }, 1000);
-                $("div.block-wsmap").animate({
-                    'height':originalMapBlockHeight + 'px'
-                }, 1000);
+        return false;
+    });
 
-            }
+    $("#collapse_map").click(function(){
+        $("body.with-highlight #navigation .section").hide(1000);
+        $(".region-sidebar-first .section").show(1000);
+        $(".region-highlight").show(1000);
+        $(".sidebar-first #content").animate({
+            marginLeft: '240px',
+            width: '720px'
+        }, 1000, function(){
+            $("#expand_map").show();
+            $("#collapse_map").hide();
+            $("body.with-highlight #navigation .section").css("top","-260px");
+            $("body.with-highlight #navigation .section").show("fast");
+        });
+
+        // If the height was expanded on expansion, let's collapse to original height
+        if($('#wsmap_map').height() > originalMapHeight){
+            $("#wsmap_map").animate({
+                'height':originalMapHeight + 'px'
+            }, 1000);
+            $("#content .block-wsmap").animate({
+                'height':originalMapBlockHeight + 'px'
+            }, 1000);
+
         }
+
             return false;
     });
 }
