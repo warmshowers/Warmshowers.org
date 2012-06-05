7d8e237ea4a67a108563aea15fc80171f5aac907
Chris Russo <chris.andrews.russo@gmail.com>
2012-06-05 16:00:25 -0400

    Remove breadcrumb variable from profile pages

--- sites/all/themes/dev/warmshowers_zen/template.php
+++ sites/all/themes/dev/warmshowers_zen/template.php
@@ -81,6 +81,11 @@ function warmshowers_zen_preprocess_page(&$variables) {
   if (!empty($variables['highlight'])) {
     $variables['classes_array'][] = 'with-highlight';
   }
+
+  // Remove breadcrumb from profile pages, but don't remove from template for forums and perhaps other places.
+  if (($url_parts = explode("/", $_GET['q'])) && $url_parts[0] == 'user') {
+    unset($variables['breadcrumb']);
+  }
 }
 
 /**
