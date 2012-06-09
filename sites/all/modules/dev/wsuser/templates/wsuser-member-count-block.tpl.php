1d8438749195f5b0f1102e2a6f7a451587f40615
Randy Fay <randy@randyfay.com>
2012-06-09 12:55:09 -0600

    Move queries from tpl.php into template_preprocess() per #68. Pacify Chris.

--- sites/all/modules/dev/wsuser/templates/wsuser-member-count-block.tpl.php
+++ sites/all/modules/dev/wsuser/templates/wsuser-member-count-block.tpl.php
@@ -6,21 +6,20 @@
  * member summary page.
  *
  * Note that this block is set to be cached globally, so counts on block caching being turned on.
+ *
+ * Variables:
+ * - $numusers: Total number of users currently on the site
+ * - $numhosts: Total number of users currently hosting.
  */
-
-  $numusers = db_result(db_query("SELECT COUNT(*) numusers FROM {users} u,{wsuser} w
-          WHERE u.uid = w.uid AND u.uid>1 AND u.status AND !isunreachable AND !isstale"));
-  $numhosts = db_result(db_query("SELECT COUNT(*) numusers FROM {users} u,{wsuser} w
-          WHERE u.uid = w.uid AND u.uid>1 AND u.status AND !isunreachable AND !isstale AND !notcurrentlyavailable"));
 ?>
-  <div class="numusers">
-    <span class="big_number"><?php print number_format($numusers); ?></span>
-    <br />
-    <?php print t('Active Members'); ?>
-  </div>
-  <div class="numhosts">
-    <span class="big_number"><?php print number_format($numhosts); ?></span>
-    <br />
-    <?php print t('Active Hosts'); ?>
-  </div>
+<div class="numusers">
+  <span class="big_number"><?php print number_format($numusers); ?></span>
+  <br />
+  <?php print t('Active Members'); ?>
+</div>
+<div class="numhosts">
+  <span class="big_number"><?php print number_format($numhosts); ?></span>
+  <br />
+  <?php print t('Active Hosts'); ?>
+</div>
 
