a223be78916e561866bc48f629f7e4f71fca3f1e
Chris Russo <chris.andrews.russo@gmail.com>
2012-06-18 12:29:51 -0400

    Add parent target for iframe map links, #48
    Also reintroduce styles so collapse map link is hidden

--- sites/all/themes/dev/warmshowers_zen/templates/page-maponly.tpl.php
+++ sites/all/themes/dev/warmshowers_zen/templates/page-maponly.tpl.php
@@ -111,9 +111,10 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
 
 <head>
+  <base target="_parent" />
   <title><?php print $head_title; ?></title>
   <?php print $head; ?>
-  <?php // print $styles; ?>
+  <?php print $styles; ?>
   <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>">
