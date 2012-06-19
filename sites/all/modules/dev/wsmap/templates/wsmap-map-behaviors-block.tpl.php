2d186281ba8aa59e18aae6ec53db5485ceddb724
Chris Russo <chris.andrews.russo@gmail.com>
2012-06-18 22:52:58 -0400

    Change capitalization for translation

--- sites/all/modules/dev/wsmap/templates/wsmap-map-behaviors-block.tpl.php
+++ sites/all/modules/dev/wsmap/templates/wsmap-map-behaviors-block.tpl.php
@@ -1,5 +1,5 @@
-<a href="#" id="expand_map"><?php print t("Expand map"); ?></a>
+<a href="#" id="expand_map"><?php print t("Expand Map"); ?></a>
 <br />
 <input id="adv_cyc_checkbox" type="checkbox" />
-  <?php echo t('Load US Adventure Cycling routes'); ?>
+  <?php echo t('Load US Adventure Cycling Routes'); ?>
 
