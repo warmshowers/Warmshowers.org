a26206ce38a484ae3d81164009c9091b593b8ca5
Randy Fay <randy@randyfay.com>
2013-01-15 11:10:37 -0700

    Added maps/search help link for #21

--- sites/all/modules/dev/wsmap/templates/wsmap-map-behaviors-block.tpl.php
+++ sites/all/modules/dev/wsmap/templates/wsmap-map-behaviors-block.tpl.php
@@ -3,6 +3,7 @@
  * Template for the map behaviors block.
  */
 ?>
+<a href="/searching" target="_blank"><?php print t('Need help searching?'); ?></a><br/>
 <a href="#" id="expand_map"><?php print t("Expand Map"); ?></a>
 <br />
 <input id="adv_cyc_checkbox" type="checkbox" />
