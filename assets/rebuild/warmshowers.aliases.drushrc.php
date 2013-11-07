2e4828e8e406b6c74715dbecf15ced20079520c2
Kosta Harlan <kostajh@gmail.com>
2013-11-07 10:56:19 -0500

    Add TODO items for users who are copying the example alias

--- assets/rebuild/warmshowers.aliases.drushrc.php
+++ assets/rebuild/warmshowers.aliases.drushrc.php
@@ -6,26 +6,25 @@
  */
 
 /**
- * This is an example of an alias for a local dev environment.
+ * This is an example alias for a warmshowers.dev local environment.
  *
  * You can learn quite a bit about Drush aliases by accessing
  * `drush docs-aliases`.
+ *
+ * There are three TODOs listed below. Make sure you attend to all of them!
  */
-$aliases['local'] = array(
-  'root' => '/home/kosta/src/warmshowers.org',
-  'uri' => 'https://local.warmshowers.org',
-  'db-url' => 'mysql://root@localhost/warmshowers_local',
+// TODO: Edit the 'root' value to match the path to your local site.
+// Don't add a trailing slash to the $site_root variable.
+$site_root = '/home/kosta/src/warmshowers.org';
+$aliases['dev'] = array(
+  'root' => $site_root,
+  'uri' => 'http://warmshowers.dev',
+  // TODO: Make sure you have created a local database called 'warmshowers'.
+  'db-url' => 'mysql://root@localhost/warmshowers',
   'path-aliases' => array(
-    // Under path aliases, you specify the full path to the rebuild manifest
-    // for your local environment.
-    '%rebuild' => '/home/kosta/.drush/warmshowers.rebuild.yaml',
+    '%rebuild' => sprintf('%s/assets/rebuild/warmshowers.rebuild.yaml', $site_root),
   ),
-  // In the rebuild section of your alias, you can define variables to replace
-  // placeholders in your manifest file.
-  //
-  // For example, if you had variable[site_mail] = %email in your rebuild
-  // manifest, then the value here would be swapped with the placeholder during
-  // the rebuild.
+  // TODO: Set your email address in the 'email' section below.
   '#rebuild' => array(
     'email' => 'youremailhere@localhost.com',
   ),
