ad58c30841841924280412427ae20a8c2141c094
Dan Murphy <daniel.murphy.jr@gmail.com>
2013-11-26 12:00:15 -0500

    Add initial condition for displaying the date field.
    Initially, only display the the date field if the 'Not Currently Available'
    checkbox is checked. RE: Issue #357

--- docroot/sites/all/modules/dev/wsuser/js/profile.js
+++ docroot/sites/all/modules/dev/wsuser/js/profile.js
@@ -8,6 +8,9 @@ Drupal.behaviors.profileBehaviors = function(context) {
 
     // Show hide "Become available" date based on whether
     // currently unavailable is checked or not
+    if (!$('#edit-notcurrentlyavailable').is(':checked')) {
+        $("#edit-becomeavailable-wrapper").hide();
+    }
     $('#edit-notcurrentlyavailable').click(function () {
         $("#edit-becomeavailable-wrapper").toggle(this.checked);
     });
