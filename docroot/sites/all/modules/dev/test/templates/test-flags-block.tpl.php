686d1319aa343b318764de496d3d1f32c3ecebab
boris blondin <blondin.boris@gmail.com>
2014-02-23 12:04:51 +0700

    flags test 2

--- docroot/sites/all/modules/dev/test/templates/test-flags-block.tpl.php
+++ docroot/sites/all/modules/dev/test/templates/test-flags-block.tpl.php
@@ -3,29 +3,12 @@
  * Member actions block template
  *
  * Supported variables
- * - $account (not sanitized)
- * - $uid
- * - $username
- * - $fullname (User's full name from wsuser)
- * - $homephone
- * - $mobilephone
- * - $homephone
- * - $street
- * - $additional
- * - $city
- * - $province
- * - $country
- * - $postal_code
- * - $latitude
- * - $longitude
- * - $responsive_member: Count of times marked responsive
- * - $unresponsive_member : Count of times marked unresponsive
- * @see wsuser_preprocess_wsuser_member_contact_location()
+ * - $user_id
  */
 
 ?>
 
 <?php if (!$is_self):
-  print flag_create_link('i_hosted_this_member', $uid);
+  print flag_create_link('i_hosted_this_member', $user_id);
 endif;
 ?>
