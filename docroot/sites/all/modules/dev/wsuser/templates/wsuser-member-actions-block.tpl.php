0d4db4cb9551031afdae4dbff05d91728de01ec0
boris blondin <blondin.boris@gmail.com>
2014-02-25 13:03:52 +0700

    clean version with links displayed in actions block

--- docroot/sites/all/modules/dev/wsuser/templates/wsuser-member-actions-block.tpl.php
+++ docroot/sites/all/modules/dev/wsuser/templates/wsuser-member-actions-block.tpl.php
@@ -28,7 +28,18 @@
 <?php 
 if (!$is_self):
 	foreach($wsuser_member_actions_block_links as $link){
-		print theme($link[0],$link[1]);
+		if($link['type']=='flag_checkbox'){
+			$f=flag_get_flag($link['data']['flag_name']);
+			$html=flag_create_link($link['data']['flag_name'],$link['data']['user_id']);
+			if (!$f->is_flagged($link['data']['user_id']) or $f->access($link['data']['user_id'], 'unflag')){
+				//dirty code to add css classes to the link, Dont know how to do it properly with flag module but I guess it's not possible.
+				print str_replace('linkbutton','linkbutton dark big rounded',$html);
+				}
+			else{
+				print $html;
+				}
+			}
+		else print theme($link['type'],$link['data']);
 	}
 endif;
 ?>
