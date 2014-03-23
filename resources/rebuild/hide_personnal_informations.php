05b78faaaf27a8bedfc242ed5589c1ed6d43cfb2
Kosta Harlan <kostajh@gmail.com>
2014-03-23 13:51:27 -0400

    Drupal coding standards

--- resources/rebuild/hide_personnal_informations.php
+++ resources/rebuild/hide_personnal_informations.php
@@ -1,30 +1,40 @@
 #!/usr/bin/env drush
 <?php
 
+/**
+ * @file
+ * Sanitization script to remove user data.
+ */
+
 $self_record = drush_sitealias_get_record('@warmshowers.dev');
+// Sanity check.
 if (!$self_record) {
   return drush_set_error('NO_ALIAS_FOUND', dt('Failed to load your @warmshowers.dev alias.'));
 }
 
-drush_log("Updating database to hide sensitive information...",'ok');
-
-$table_user = 'users';  //table containing users. (previously used for testing)
-$table_location = 'user_location';  //table containing user's location (previously used for testing)
+drush_log('Updating database to hide sensitive information', 'ok');
+// Table containing users.
+$table_user = 'users';
+// Table containing user's location.
+$table_location = 'user_location';
+// Custom wsuser table.
 $table_wsuser = 'wsuser';
 
-$req = "update {$table_location} set
-street=CONCAT(oid,'_street'),
-city=CONCAT(oid,'_city'),
-postal_code=CONCAT(oid,'_postcode')";
-drush_invoke_process('@warmshowers.dev','sqlq',array($req));
+$req = "UPDATE {$table_location} SET
+street = CONCAT(oid, '_street'),
+city = CONCAT(oid, '_city'),
+postal_code = CONCAT(oid, '_postcode')";
 
-$req="update {$table_wsuser} set
-fullname = CONCAT(uid,'_fullname'),
-fax_number =case when fax_number='' then '' else CONCAT(uid,'_fax_number') end,
-homephone = case when homephone='' then '' else CONCAT(uid,'_homephone') end,
-mobilephone = case when mobilephone='' then '' else CONCAT(uid,'_mobilephone') end,
-workphone = case when workphone='' then '' else CONCAT(uid,'_workphone') end";
-drush_invoke_process('@warmshowers.dev','sqlq',array($req));
+drush_invoke_process('@warmshowers.dev', 'sql-query', array($req));
 
-$req="update {$table_user} set data=''";
-drush_invoke_process('@warmshowers.dev','sqlq',array($req));
+$req = "UPDATE {$table_wsuser} SET
+  fullname = CONCAT(uid,'_fullname'),
+  fax_number = case WHEN fax_number = '' THEN '' ELSE CONCAT(uid, '_fax_number') END,
+  homephone = case WHEN homephone = '' THEN '' ELSE CONCAT(uid, '_homephone') END,
+  mobilephone = case WHEN mobilephone = '' THEN '' ELSE CONCAT(uid, '_mobilephone') END,
+  workphone = case WHEN workphone = '' THEN '' ELSE CONCAT(uid, '_workphone') END";
+
+drush_invoke_process('@warmshowers.dev', 'sql-query', array($req));
+
+$req = "UPDATE {$table_user} SET data = ''";
+drush_invoke_process('@warmshowers.dev', 'sql-query', array($req));
