fc323f45287edb0ef06b6ef6241ee568f5cb0693
Randy Fay <randy@randyfay.com>
2014-12-27 14:49:25 -0700

    Update languages and sanitization scripts- no impact on code

--- resources/devsite_scripts/change_user_emails.sh
+++ resources/devsite_scripts/change_user_emails.sh
@@ -4,3 +4,4 @@ echo "This script will give dummy emails to all but a few key accounts. It's int
 db=warmshowers
 
 echo "update users set mail=concat('user_', uid, '@localhost') where uid not in (1,1165, 12075, 8088, 36456, 18358, 36076);" | mysql $db 
+echo "update users set init=mail; " | mysql $db
--- resources/devsite_scripts/clean_up_languages.sh
+++ resources/devsite_scripts/clean_up_languages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+webroot=~/workspace/warmshowers/docroot
 devsite_host=warmshowers.dev
 # If the English language version of the site has www on it, put "www." here.
 # Otherwise www. will be deleted from the language URLs.
@@ -8,7 +9,7 @@ www_prefix=""
 #drush vset language_negotiation 0
 sed "s/warmshowers.org/$devsite_host/g
 s/https/http/g
-s/www./$www_prefix/g" languages.sql  | drush sqlc
+s/www./$www_prefix/g" languages.sql  | drush sqlc -r $webroot
 drush cc all
 
 
