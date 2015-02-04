0b9b2726186130608030fe9168c03b0dd571ed2b
Randy Fay <randy@randyfay.com>
2015-02-04 15:04:28 -0800

    Improve Ubercart customer order template for #417

---
+++ docroot/sites/all/themes/dev/warmshowers_zen/templates/uc_order.tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * This file is a dummy template.
+ *
+ * It serves as a reminder to theme authors that they must have a
+ * uc_order.tpl.php file in their theme if they want to override the other
+ * order templates.
+ */
