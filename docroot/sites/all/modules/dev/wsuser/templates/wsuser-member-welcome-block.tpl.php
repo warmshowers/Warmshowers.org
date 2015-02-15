989aa9b7d546e9ff49dc5214ca158a79d27129a8
Randy Fay <randy@randyfay.com>
2015-02-15 12:44:08 -0800

    Fixes #467 by making linkbuttons on the welcome block smaller

--- docroot/sites/all/modules/dev/wsuser/templates/wsuser-member-welcome-block.tpl.php
+++ docroot/sites/all/modules/dev/wsuser/templates/wsuser-member-welcome-block.tpl.php
@@ -3,6 +3,5 @@
  * Template for member welcome block.
   */
 ?>
-<div id='welcome-text'><?php print t('Welcome'); ?></div>
 <div class='name'><?php print theme('username', $user) ?></div>
 <?php print theme('linkbuttons', $wsuser_member_welcome_block_menu_links); ?>
