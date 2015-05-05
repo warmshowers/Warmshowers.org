88327f7661cb1618421609cc4dceb742853db31e
Randy Fay <randy@randyfay.com>
2015-05-04 21:36:08 -0600

    Set map location for user now working

--- docroot/sites/all/modules/dev/user_location/user_location_setlocation.js
+++ docroot/sites/all/modules/dev/user_location/user_location_setlocation.js
@@ -3,26 +3,31 @@
  *
  * @type {Boolean}
  */
+(function ($) {
 
-Drupal.behaviors.user_location_setlocation = function () {
-  var defaultZoom = 10;
-  var lat = Drupal.settings.user_location.location.latitude;
-  var lon = Drupal.settings.user_location.location.longitude;
+  Drupal.behaviors.user_location_setlocation = {
+    attach: function (context, settings) {
 
-  var mapOptions = {
-    center:new google.maps.LatLng(lat, lon),
-    zoom:defaultZoom,
-    mapTypeId:google.maps.MapTypeId.TERRAIN
-  };
+      var defaultZoom = 10;
+      var lat = Drupal.settings.user_location.location.latitude;
+      var lon = Drupal.settings.user_location.location.longitude;
 
-  var map = new google.maps.Map(document.getElementById('locationset'), mapOptions);
-  var marker = new google.maps.Marker({
-    position:mapOptions.center,
-    map:map,
-  });
-  google.maps.event.addListener(map, 'click', function (event) {
-    $('#edit-longitude').attr('value', event.latLng.lng());
-    $('#edit-latitude').attr('value', event.latLng.lat());
-    marker.setPosition(event.latLng);
-  });
-}
+      var mapOptions = {
+        center: new google.maps.LatLng(lat, lon),
+        zoom: defaultZoom,
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+      };
+
+      var map = new google.maps.Map(document.getElementById('locationset'), mapOptions);
+      var marker = new google.maps.Marker({
+        position: mapOptions.center,
+        map: map
+      });
+      google.maps.event.addListener(map, 'click', function (event) {
+        $('#edit-longitude').attr('value', event.latLng.lng());
+        $('#edit-latitude').attr('value', event.latLng.lat());
+        marker.setPosition(event.latLng);
+      });
+    }
+  }
+})(jQuery)
