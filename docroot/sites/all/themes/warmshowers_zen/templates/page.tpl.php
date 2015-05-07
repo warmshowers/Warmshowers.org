0a36f1e62249aec739ad032b02be012c9c26b608
Anne Tomasevich <annetvich@gmail.com>
2015-05-07 10:51:03 -0400

    Issue #498: Update page.tpl.php to D7 standards

--- docroot/sites/all/themes/warmshowers_zen/templates/page.tpl.php
+++ docroot/sites/all/themes/warmshowers_zen/templates/page.tpl.php
@@ -1,258 +1,152 @@
 <?php
 /**
  * @file
- * Theme implementation to display a single Drupal page.
+ * Returns the HTML for a single Drupal page.
  *
- * Available variables:
- *
- * General utility variables:
- * - $base_path: The base URL path of the Drupal installation. At the very
- *   least, this will always default to /.
- * - $css: An array of CSS files for the current page.
- * - $directory: The directory the template is located in, e.g. modules/system
- *   or themes/garland.
- * - $is_front: TRUE if the current page is the front page. Used to toggle the mission statement.
- * - $logged_in: TRUE if the user is registered and signed in.
- * - $is_admin: TRUE if the user has permission to access administration pages.
- *
- * Page metadata:
- * - $language: (object) The language the site is being displayed in.
- *   $language->language contains its textual representation.
- *   $language->dir contains the language direction. It will either be 'ltr' or 'rtl'.
- * - $head_title: A modified version of the page title, for use in the TITLE tag.
- * - $head: Markup for the HEAD section (including meta tags, keyword tags, and
- *   so on).
- * - $styles: Style tags necessary to import all CSS files for the page.
- * - $scripts: Script tags necessary to load the JavaScript files and settings
- *   for the page.
- * - $classes: String of classes that can be used to style contextually through
- *   CSS. It should be placed within the <body> tag. When selecting through CSS
- *   it's recommended that you use the body tag, e.g., "body.front". It can be
- *   manipulated through the variable $classes_array from preprocess functions.
- *   The default values can be one or more of the following:
- *   - front: Page is the home page.
- *   - not-front: Page is not the home page.
- *   - logged-in: The current viewer is logged in.
- *   - not-logged-in: The current viewer is not logged in.
- *   - node-type-[node type]: When viewing a single node, the type of that node.
- *     For example, if the node is a "Blog entry" it would result in "node-type-blog".
- *     Note that the machine name will often be in a short form of the human readable label.
- *   - page-views: Page content is generated from Views. Note: a Views block
- *     will not cause this class to appear.
- *   - page-panels: Page content is generated from Panels. Note: a Panels block
- *     will not cause this class to appear.
- *   The following only apply with the default 'sidebar_first' and 'sidebar_second' block regions:
- *     - two-sidebars: When both sidebars have content.
- *     - no-sidebars: When no sidebar content exists.
- *     - one-sidebar and sidebar-first or sidebar-second: A combination of the
- *       two classes when only one of the two sidebars have content.
- * - $node: Full node object. Contains data that may not be safe. This is only
- *   available if the current page is on the node's primary url.
- * - $menu_item: (array) A page's menu item. This is only available if the
- *   current page is in the menu.
- *
- * Site identity:
- * - $front_page: The URL of the front page. Use this instead of $base_path,
- *   when linking to the front page. This includes the language domain or prefix.
- * - $logo: The path to the logo image, as defined in theme configuration.
- * - $site_name: The name of the site, empty when display has been disabled
- *   in theme settings.
- * - $site_slogan: The slogan of the site, empty when display has been disabled
- *   in theme settings.
- * - $mission: The text of the site mission, empty when display has been disabled
- *   in theme settings.
- *
- * Navigation:
- * - $search_box: HTML to display the search box, empty if search has been disabled.
- * - $primary_links (array): An array containing the Primary menu links for the
- *   site, if they have been configured.
- * - $secondary_links (array): An array containing the Secondary menu links for
- *   the site, if they have been configured.
- * - $breadcrumb: The breadcrumb trail for the current page.
- *
- * Page content (in order of occurrence in the default page.tpl.php):
- * - $title: The page title, for use in the actual HTML content.
- * - $messages: HTML for status and error messages. Should be displayed prominently.
- * - $tabs: Tabs linking to any sub-pages beneath the current page (e.g., the
- *   view and edit tabs when displaying a node).
- * - $help: Dynamic help text, mostly for admin pages.
- * - $content: The main content of the current page.
- * - $feed_icons: A string of all feed icons for the current page.
- *
- * Footer/closing data:
- * - $footer_message: The footer message as defined in the admin settings.
- * - $closure: Final closing markup from any modules that have altered the page.
- *   This variable should always be output last, after all other dynamic content.
- *
- * Helper variables:
- * - $classes_array: Array of html class attribute values. It is flattened
- *   into a string within the variable $classes.
- *
- * Regions:
- * - $content_top: Items to appear above the main content of the current page.
- * - $content_bottom: Items to appear below the main content of the current page.
- * - $navigation: Items for the navigation bar.
- * - $sidebar_first: Items for the first sidebar.
- * - $sidebar_second: Items for the second sidebar.
- * - $header: Items for the header region.
- * - $footer: Items for the footer region.
- * - $page_closure: Items to appear below the footer.
- *
- * The following variables are deprecated and will be removed in Drupal 7:
- * - $body_classes: This variable has been renamed $classes in Drupal 7.
- *
- * @see template_preprocess()
- * @see template_preprocess_page()
- * @see zen_preprocess()
- * @see zen_process()
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728148
  */
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
 
-<head>
-  <title><?php print $head_title; ?></title>
-  <?php print $head; ?>
-  <?php print $styles; ?>
-  <?php print $scripts; ?>
-</head>
-<body class="<?php print $classes; ?>">
+<div id="header-style" class="<?php print $classes; ?>"><div id="header-wrapper">
 
-  <?php if ($primary_links): ?>
-    <div id="skip-link"><a href="#main-menu"><?php print t('Jump to Navigation'); ?></a></div>
-  <?php endif; ?>
-
-  <div id="header-style" class="<?php print $classes; ?>"><div id="header-wrapper">
-
-    <div id="header"><div class="section clearfix">
-      <div id="logotitle">
+  <div id="header"><div class="section clearfix">
+    <div id="logotitle">
       <?php if ($logo): ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
       <?php endif; ?>
 
       <?php if ($site_name || $site_slogan): ?>
         <div id="name-and-slogan">
-        <?php if ($title): ?>
-          <div id="site-name"><strong>
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print t("Warm Showers"); ?></span></a>
-          </strong></div>
-        <?php else: /* Use h1 when the content title is empty */ ?>
-          <h1 id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print t("Warm Showers"); ?></span></a>
-          </h1>
-        <?php endif; ?>
+          <?php if ($title): ?>
+            <div id="site-name"><strong>
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print t("Warm Showers"); ?></span></a>
+            </strong></div>
+          <?php else: /* Use h1 when the content title is empty */ ?>
+            <h1 id="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print t("Warm Showers"); ?></span></a>
+            </h1>
+          <?php endif; ?>
 
           <div id="site-slogan"><?php print t("A community for touring cyclists and hosts"); ?></div>
         </div><!-- /#name-and-slogan -->
       <?php endif; ?>
+    </div>
+
+    <div class="signpost">
+      <div class="signpost_txt">
+        <?php print t('Hospitality Ahead'); ?>
       </div>
-      <?php if ($search_box): ?>
-        <div id="search-box"><?php print $search_box; ?></div>
+      <div id="hospitality">
+        <?php print t("We are built on 100% Reciprocal Hospitality!"); ?>
+      </div>
+    </div>
+
+    <div id="authentication_block" <?php if ($logged_in): ?>class="logged_in"<?php endif; ?>>
+      <div id="authentication_block_wrapper" class="clearfix">
+        <?php print $authentication_block; ?>
+      </div>
+    </div>
+
+    <?php print render($page['header']); ?>
+
+  </div></div><!-- /.section, /#header -->
+</div></div> <!-- /#header-wrapper, /#header-style -->
+
+<div id="page-wrapper" class="<?php print $classes; ?>"><div id="page">
+
+  <?php if ($is_front && !$logged_in) {
+    echo '<div id="highlight_wrapper_anon">';
+    print render($page['highlighted']);
+    echo '<div class="welcome_msg"><div class="welcome_msg_wrapper">'.t('<strong>The Warm Showers Community</strong> is a free worldwide hospitality exchange for touring cyclists. People who are willing to host touring cyclists sign up and provide their contact information, and may occasionally have someone stay with them and share great stories and a drink.').'</div></div>';
+    echo '</div>';
+  } else {
+    print render($page['highlighted']);
+  } ?>
+
+  <div id="main-wrapper"><div id="main" class="clearfix<?php if ($main_menu || $navigation) { print ' with-navigation'; } ?>">
+
+    <div id="content" class="column"><div class="section">
+      <?php if (preg_match("/^(admin|forum)/", $menu_item['path']) || $node->type == 'forum') { print $breadcrumb; } ?>
+      <?php print render($title_prefix); ?>
+      <?php if ($title && !$is_front): ?>
+        <h1 class="title"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <!-- We will show the tabs only if not on the profile page) -->
+      <?php if ($tabs && (!empty($menu_item['path']) && $menu_item['path'] != 'user/%')): ?>
+        <div class="tabs"><?php print render($tabs); ?></div>
+      <?php endif; ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
 
-      <div class="signpost">
-        <div class="signpost_txt">
-            <?php print t('Hospitality Ahead'); ?>
-        </div>
-        <div id="hospitality">
-          <?php print t("We are built on 100% Reciprocal Hospitality!"); ?>
-        </div>
+      <?php print render($page['content_top']); ?>
+
+      <div id="content-area">
+        <?php print render($page['content']); ?>
       </div>
 
-      <div id="authentication_block" <?php if ($logged_in): ?>class="logged_in"<?php endif; ?>>
-        <div id="authentication_block_wrapper" class="clearfix">
-          <?php print $authentication_block; ?>
-        </div>
-      </div>
+      <?php print render($page['content_bottom']); ?>
 
-      <?php print $header; ?>
+      <?php if ($feed_icons): ?>
+        <div class="feed-icons"><?php print $feed_icons; ?></div>
+      <?php endif; ?>
 
-    </div></div><!-- /.section, /#header -->
+    </div></div><!-- /.section, /#content -->
 
-  </div></div> <!-- /#header-wrapper, /#header-style -->
-  <div id="page-wrapper" class="<?php print $classes; ?>"><div id="page">
-
-    <?php if ($is_front && !$logged_in) {
-        echo '<div id="highlight_wrapper_anon">';
-        print $highlight;
-        echo '<div class="welcome_msg"><div class="welcome_msg_wrapper">'.t('<strong>The Warm Showers Community</strong> is a free worldwide hospitality exchange for touring cyclists. People who are willing to host touring cyclists sign up and provide their contact information, and may occasionally have someone stay with them and share great stories and a drink.').'</div></div>';
-        echo '</div>';
-    } else {
-        print $highlight;
-    } ?>
-
-    <div id="main-wrapper"><div id="main" class="clearfix<?php if ($primary_links || $navigation) { print ' with-navigation'; } ?>">
-
-      <div id="content" class="column"><div class="section">
-        <?php if (preg_match("/^(admin|forum)/", $menu_item['path']) || $node->type == 'forum') { print $breadcrumb; } ?>
-        <?php if ($title && !$is_front): ?>
-          <h1 class="title"><?php print $title; ?></h1>
-        <?php endif; ?>
-        <?php print $messages; ?>
-        <!-- We will show the tabs only if not on the profile page) -->
-        <?php if ($tabs && (!empty($menu_item['path']) && $menu_item['path'] != 'user/%')): ?>
-          <div class="tabs"><?php print $tabs; ?></div>
-        <?php endif; ?>
-        <?php print $help; ?>
-
-        <?php print $content_top; ?>
-
-        <div id="content-area">
-          <?php print $content; ?>
-        </div>
-
-        <?php print $content_bottom; ?>
-
-        <?php if ($feed_icons): ?>
-          <div class="feed-icons"><?php print $feed_icons; ?></div>
-        <?php endif; ?>
-
-      </div></div><!-- /.section, /#content -->
-
-      <?php if ($primary_links || $navigation): ?>
-        <div id="navigation"><div class="section clearfix">
-
-          <?php print theme(array('links__system_main_menu', 'links'), $primary_links,
-            array(
+    <?php if ($main_menu || $navigation): ?>
+      <div id="navigation"><div class="section clearfix">
+        <?php print theme(array('links__system_main_menu'), $main_menu,
+          array(
+            'links' => $main_menu,
+            'attributes' => array(
               'id' => 'main-menu',
-              'class' => 'links clearfix',
+              'class' => array('links', 'clearfix'),
             ),
-            array(
+            'heading' => array(
               'text' => t('Main menu'),
               'level' => 'h2',
-              'class' => 'element-invisible',
-            ));
-          ?>
+              'class' => array('element-invisible'),
+            ),
+          ));
+        ?>
 
-          <?php print $navigation; ?>
+        <?php print render($page['navigation']); ?>
 
-        </div></div><!-- /.section, /#navigation -->
-      <?php endif; ?>
-
-      <?php print $sidebar_first; ?>
-
-      <?php print $sidebar_second; ?>
-
-    </div></div><!-- /#main, /#main-wrapper -->
-
-    <?php if ($footer || $footer_message || $secondary_links): ?>
-      <div id="footer" class="<?php print $classes; ?>"><div class="section">
-        <h2 class="title" dir=""><?php print t('Hospitality For Touring Cyclists Worldwide'); ?> </h2>
-        <ul class="social">
-            <li><a href="https://www.facebook.com/groups/135049549858210/" class="social_fb" title="<?php print t("Follow Warm Showers on"); ?> Facebook"></a></li>
-            <li><a href="https://twitter.com/#!/warmshowers" class="social_tw" title="<?php print t("Follow Warm Showers on"); ?> Twitter"></a></li>
-            <li><a href="https://www.warmshowers.org/rssfeed" class="social_rs" title="<?php print t("Keep up to date with"); ?> RSS"></a></li>
-        </ul>
-        <?php print $footer; ?>
-
-      </div></div><!-- /.section, /#footer -->
+      </div></div><!-- /.section, /#navigation -->
     <?php endif; ?>
 
-  </div></div><!-- /#page, /#page-wrapper -->
+    <?php
+      // Render the sidebars to see if there's anything in them.
+      $sidebar_first  = render($page['sidebar_first']);
+      $sidebar_second = render($page['sidebar_second']);
+    ?>
 
-  <?php print $page_closure; ?>
+    <?php if ($sidebar_first || $sidebar_second): ?>
+      <aside class="sidebars">
+        <?php print $sidebar_first; ?>
+        <?php print $sidebar_second; ?>
+      </aside>
+    <?php endif; ?>
 
-  <?php print $closure; ?>
+  </div></div><!-- /#main, /#main-wrapper -->
 
-</body>
-</html>
+  <?php if ($footer || $secondary_menu): ?>
+    <div id="footer" class="<?php print $classes; ?>"><div class="section">
+      <h2 class="title" dir=""><?php print t('Hospitality For Touring Cyclists Worldwide'); ?> </h2>
+      <ul class="social">
+        <li><a href="https://www.facebook.com/groups/135049549858210/" class="social_fb" title="<?php print t("Follow Warm Showers on"); ?> Facebook"></a></li>
+        <li><a href="https://twitter.com/#!/warmshowers" class="social_tw" title="<?php print t("Follow Warm Showers on"); ?> Twitter"></a></li>
+        <li><a href="https://www.warmshowers.org/rssfeed" class="social_rs" title="<?php print t("Keep up to date with"); ?> RSS"></a></li>
+      </ul>
+      <?php print render($page['footer']); ?>
+
+    </div></div><!-- /.section, /#footer -->
+  <?php endif; ?>
+
+</div></div><!-- /#page, /#page-wrapper -->
+
+<?php print render($page['bottom']); ?>
