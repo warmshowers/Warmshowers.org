967eb3100c63ff2dc4afcbcde1a2ea1d5fd726d1
Anne Tomasevich <annetvich@gmail.com>
2015-05-07 10:01:22 -0400

    Issue #498: Add D6 template files

---
+++ docroot/sites/all/themes/warmshowers_zen/templates/page-maponly.tpl.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * @file
+ * Theme implementation to display a single Drupal page.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - $base_path: The base URL path of the Drupal installation. At the very
+ *   least, this will always default to /.
+ * - $css: An array of CSS files for the current page.
+ * - $directory: The directory the template is located in, e.g. modules/system
+ *   or themes/garland.
+ * - $is_front: TRUE if the current page is the front page. Used to toggle the mission statement.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $is_admin: TRUE if the user has permission to access administration pages.
+ *
+ * Page metadata:
+ * - $language: (object) The language the site is being displayed in.
+ *   $language->language contains its textual representation.
+ *   $language->dir contains the language direction. It will either be 'ltr' or 'rtl'.
+ * - $head_title: A modified version of the page title, for use in the TITLE tag.
+ * - $head: Markup for the HEAD section (including meta tags, keyword tags, and
+ *   so on).
+ * - $styles: Style tags necessary to import all CSS files for the page.
+ * - $scripts: Script tags necessary to load the JavaScript files and settings
+ *   for the page.
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It should be placed within the <body> tag. When selecting through CSS
+ *   it's recommended that you use the body tag, e.g., "body.front". It can be
+ *   manipulated through the variable $classes_array from preprocess functions.
+ *   The default values can be one or more of the following:
+ *   - front: Page is the home page.
+ *   - not-front: Page is not the home page.
+ *   - logged-in: The current viewer is logged in.
+ *   - not-logged-in: The current viewer is not logged in.
+ *   - node-type-[node type]: When viewing a single node, the type of that node.
+ *     For example, if the node is a "Blog entry" it would result in "node-type-blog".
+ *     Note that the machine name will often be in a short form of the human readable label.
+ *   - page-views: Page content is generated from Views. Note: a Views block
+ *     will not cause this class to appear.
+ *   - page-panels: Page content is generated from Panels. Note: a Panels block
+ *     will not cause this class to appear.
+ *   The following only apply with the default 'sidebar_first' and 'sidebar_second' block regions:
+ *     - two-sidebars: When both sidebars have content.
+ *     - no-sidebars: When no sidebar content exists.
+ *     - one-sidebar and sidebar-first or sidebar-second: A combination of the
+ *       two classes when only one of the two sidebars have content.
+ * - $node: Full node object. Contains data that may not be safe. This is only
+ *   available if the current page is on the node's primary url.
+ * - $menu_item: (array) A page's menu item. This is only available if the
+ *   current page is in the menu.
+ *
+ * Site identity:
+ * - $front_page: The URL of the front page. Use this instead of $base_path,
+ *   when linking to the front page. This includes the language domain or prefix.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $site_name: The name of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $site_slogan: The slogan of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $mission: The text of the site mission, empty when display has been disabled
+ *   in theme settings.
+ *
+ * Navigation:
+ * - $search_box: HTML to display the search box, empty if search has been disabled.
+ * - $primary_links (array): An array containing the Primary menu links for the
+ *   site, if they have been configured.
+ * - $secondary_links (array): An array containing the Secondary menu links for
+ *   the site, if they have been configured.
+ * - $breadcrumb: The breadcrumb trail for the current page.
+ *
+ * Page content (in order of occurrence in the default page.tpl.php):
+ * - $title: The page title, for use in the actual HTML content.
+ * - $messages: HTML for status and error messages. Should be displayed prominently.
+ * - $tabs: Tabs linking to any sub-pages beneath the current page (e.g., the
+ *   view and edit tabs when displaying a node).
+ * - $help: Dynamic help text, mostly for admin pages.
+ * - $content: The main content of the current page.
+ * - $feed_icons: A string of all feed icons for the current page.
+ *
+ * Footer/closing data:
+ * - $footer_message: The footer message as defined in the admin settings.
+ * - $closure: Final closing markup from any modules that have altered the page.
+ *   This variable should always be output last, after all other dynamic content.
+ *
+ * Helper variables:
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ *
+ * Regions:
+ * - $content_top: Items to appear above the main content of the current page.
+ * - $content_bottom: Items to appear below the main content of the current page.
+ * - $navigation: Items for the navigation bar.
+ * - $sidebar_first: Items for the first sidebar.
+ * - $sidebar_second: Items for the second sidebar.
+ * - $header: Items for the header region.
+ * - $footer: Items for the footer region.
+ * - $page_closure: Items to appear below the footer.
+ *
+ * The following variables are deprecated and will be removed in Drupal 7:
+ * - $body_classes: This variable has been renamed $classes in Drupal 7.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ * @see zen_preprocess()
+ * @see zen_process()
+ */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
+
+<head>
+  <base target="_parent" />
+  <title><?php print $head_title; ?></title>
+  <?php print $head; ?>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+</head>
+<body class="<?php print $classes; ?>">
+  <div id="content-area">
+    <?php print $content; ?>
+  </div>
+  <?php print $page_closure; ?>
+
+
+</body>
+</html>
---
+++ docroot/sites/all/themes/warmshowers_zen/templates/user-profile.tpl.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * @file user-profile.tpl.php
+ *
+ * warmshowers_zen version of user profile theming.
+ *
+ * Available variables
+ * - $uid
+ * - $username
+ * - $fullname
+ * - $homephone
+ * - $mobilephone
+ * - $workphone
+ * - $street
+ * - $additional
+ * - $city
+ * - $province
+ * - $country
+ * - $postal_code
+ * - $latitude
+ * - $longitude
+ * - $reference_count
+ * - $last_login
+ * - $responsive_member
+ * - $member_hosted_me
+ * - $services
+ * - $URL
+ * - $motel, $bikeshop, $maxcyclists, $campground, $languagesspoken
+ * - $global_stats - array()
+ * - $personal_stats - array()
+  */
+?>
+<?php drupal_set_title(check_plain($account->fullname)); ?>
+<div id="profile-container">
+  <div id="profile-top">
+    <div id="profile-image"><?php
+      if (!empty($photo_scolding)) {
+        print $photo_scolding;
+      } else {
+        print theme('user_picture', $account);
+      } ?></div>
+    <div id="name-title">
+      <h3><?php print $fullname; ?></h3>
+
+      <ul id="user_stats"><?php $i=0; foreach ($global_stats as $classname=>$stat){
+        $i++;
+        $classname .= " number";
+            if ($i == 0) { $classname .= " leaf-first"; }
+        ?><li class="<?php print $classname; ?>"><?php print $stat; ?></li><?php
+      }
+        $i=0;
+        if ($personal_stats) {
+          foreach ($personal_stats as $classname=>$stat) {
+        $i++;
+        $classname .= " personal";
+            if ($i == count($personal_stats)) {
+              $classname .= " leaf-last";
+            }
+            print '<li class="' . $classname . '">' . $stat . '</li>';
+          }
+        }
+    ?></ul>
+    </div>
+    <div id="profile-tabs">
+      <?php print $menu_primary_local_tasks; ?>
+    </div>
+  </div>
+  <div class="content">
+    <h1><?php print t('About this Member'); ?></h1>
+    <div id="account-comments"><?php print check_markup($account->comments); ?></div>
+    <div id="host-services">
+      <h2><?php print t('Hosting information'); ?></h2>
+      <?php if ($notcurrentlyavailable) : ?>
+        <?php print t('This member has marked themselves as not currently available for hosting, so their hosting information is not displayed. <br/>Expected return @return.', array('@return' => $return_date)); ?>
+      <?php else: ?>
+        <?php foreach (array('preferred_notice', 'maxcyclists', 'bikeshop', 'campground', 'motel') as $item) : ?>
+           <?php if (!empty($$item)): ?>
+             <div class="member-info-<?php print $item; ?>"><span class="item-title"><?php print $fieldlist[$item]['title'];?></span>: <span class="item-value"><?php print $$item; ?></span></div>
+           <?php endif; ?>
+        <?php endforeach; ?>
+        <h4><?php print t('This host may offer'); ?></h4>
+        <ul>
+          <?php print $services; ?>
+        </ul>
+      <?php endif; ?>
+    </div>
+    <div id="recommendations">
+      <h2><?php print t('Feedback'); ?></h2>
+      <?php print views_embed_view('user_referrals_by_referee', 'block_1', $account->uid); ?>
+    </div>
+  </div>
+</div>
