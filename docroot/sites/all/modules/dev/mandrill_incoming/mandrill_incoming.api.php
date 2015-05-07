190beaf523336da1c74034cd8192a082fc175fc7
Kosta Harlan <kosta@kostaharlan.net>
2015-05-07 11:33:10 -0400

    Issue #496: Drupal coding standards
    - Apply Drupal coding standards
    - Move the example hook implementation into a separate file.

---
+++ docroot/sites/all/modules/dev/mandrill_incoming/mandrill_incoming.api.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by Mandrill Incoming for interacting with incoming events.
+ */
+
+/**
+ * @addtogroup hooks
+ */
+
+/**
+ * Example implementation of hook_mandrill_incoming_event().
+ *
+ * @param object $event
+ *   The event object format is described at http://help.mandrill.com/entries/22092308-What-is-the-format-of-inbound-email-webhooks-.
+ *
+ * @return int
+ *   MANDRILL_INCOMING_HANDLED
+ *   MANDRILL_INCOMING_UNHANDLED
+ *   MANDRILL_INCOMING_ERROR
+ */
+function hook_mandrill_incoming_event($event) {
+  $msg = $event->msg;
+  watchdog('mandrill_incoming', 'Received "inbound" from=%from to=%to subject=%subject',
+    array(
+      '%from' => print_r($msg->from_email, TRUE),
+      '%to' => print_r($msg->to, TRUE),
+      '%subject' => $msg->subject,
+    )
+  );
+  return MANDRILL_INCOMING_UNHANDLED;
+}
