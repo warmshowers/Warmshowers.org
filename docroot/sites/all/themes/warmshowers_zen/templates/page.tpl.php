119bce5811a52aebfeeac9d84eb9802d0212584b
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2015-06-22 00:09:56 +0100

    D7 theme upgrade second pass.

--- docroot/sites/all/themes/warmshowers_zen/templates/page.tpl.php
+++ docroot/sites/all/themes/warmshowers_zen/templates/page.tpl.php
@@ -9,21 +9,22 @@
 ?>
 <div id="page-container" class="<?php print $classes; ?>">
 
-  <div id="header-wrapper" class="header wrapper">
-    <header id="header" class="container responsive">
-      <div class="site-logo wrapper">
+  <div id="header-wrapper" class="header-wrapper">
+    <header id="header" class="header container responsive" role="banner">
+
+      <div class="header__site-logo wrapper">
         <?php if ($logo): ?>
-          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo" class="logo">
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo" class="header__logo">
             <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
           </a>
         <?php endif; ?>
 
         <?php if ($site_name || $site_slogan): ?>
-          <div class="site-name-and-slogan">
-            <h1 class="site-name">
+          <div class="header__site-name-and-slogan">
+            <h1 class="header__site-name">
               <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print t('Warm Showers'); ?></span></a>
             </h1>
-            <div class="site-slogan"><?php print t('A community for touring cyclists and hosts'); ?></div>
+            <div class="header__site-slogan" id="site-slogan"><?php print t('A community for touring cyclists and hosts'); ?></div>
           </div>
         <?php endif; ?>
       </div>
@@ -31,7 +32,7 @@
       <?php print render($page['header']); ?>
 
       <?php if (!$logged_in): ?>
-        <div class="signpost wrapper">
+        <div class="header__signpost wrapper">
           <div class="signpost_txt">
             <?php print t('Hospitality Ahead'); ?>
           </div>
@@ -42,25 +43,27 @@
       <?php endif; ?>
 
       <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-        <div class="primary-navbar navbar">
-          <nav role="navigation">
+        <div class="header__primary-navbar navbar">
+          <nav class="header__primary-menu" id="primary-menu" role="navigation">
             <?php if (!empty($primary_nav)): ?>
               <?php print render($primary_nav); ?>
             <?php endif; ?>
+          </nav>
+          <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
             <?php if (!empty($secondary_nav)): ?>
               <?php print render($secondary_nav); ?>
             <?php endif; ?>
-            <?php if (!empty($page['navigation'])): ?>
-              <?php print render($page['navigation']); ?>
-            <?php endif; ?>
           </nav>
+          <?php if (!empty($page['navigation'])): ?>
+            <?php print render($page['navigation']); ?>
+          <?php endif; ?>
         </div>
       <?php endif; ?>
 
     </header>
   </div>
 
-  <div id="page-wrapper" class="page wrapper">
+  <div id="page-wrapper" class="page-wrapper">
     <section id="page" class="container">
 
       <?php if (!empty($page['highlighted'])): ?>
@@ -75,7 +78,9 @@
 
         <section id="main" class="main">
 
-          <div class="tabs"><?php print render($tabs); ?></div>
+          <div id="tab-area" class="tab-area">
+            <?php print render($tabs); ?>
+          </div>
 
           <div id="content-area" class="content-area">
             <?php if (preg_match("/^(admin|forum)/", $menu_item['path']) || $node->type == 'forum') { print $breadcrumb; } ?>
@@ -89,6 +94,7 @@
             <?php print $messages; ?>
 
             <?php print render($page['help']); ?>
+
             <?php if ($action_links): ?>
               <ul class="action-links"><?php print render($action_links); ?></ul>
             <?php endif; ?>
@@ -112,15 +118,17 @@
           </aside>
       </div><!-- /#main-wrapper -->
 
-      <div id="footer-wrapper" class="footer wrapper">
-        <footer id="footer" class="container responsive">
-          <h2 class="footer-title" dir=""><?php print t('Hospitality For Touring Cyclists Worldwide'); ?> </h2>
+      <div id="footer-wrapper" class="footer-wrapper">
+        <footer id="footer" class="footer container responsive">
+          <h2 class="footer__title" dir=""><?php print t('Hospitality For Touring Cyclists Worldwide'); ?> </h2>
 
-          <ul class="social">
-            <li><a href="https://www.facebook.com/groups/135049549858210/" class="social_fb" title="<?php print t("Follow Warm Showers on"); ?> Facebook"></a></li>
-            <li><a href="https://twitter.com/#!/warmshowers" class="social_tw" title="<?php print t("Follow Warm Showers on"); ?> Twitter"></a></li>
-            <li><a href="https://www.warmshowers.org/rssfeed" class="social_rs" title="<?php print t("Keep up to date with"); ?> RSS"></a></li>
-          </ul>
+          <nav class="footer__social-menu">
+            <ul class="menu">
+              <li><a href="https://www.facebook.com/groups/135049549858210/" class="social_fb" title="<?php print t("Follow Warm Showers on"); ?> Facebook"></a></li>
+              <li><a href="https://twitter.com/#!/warmshowers" class="social_tw" title="<?php print t("Follow Warm Showers on"); ?> Twitter"></a></li>
+              <li><a href="https://www.warmshowers.org/rssfeed" class="social_rs" title="<?php print t("Keep up to date with"); ?> RSS"></a></li>
+            </ul>
+          </nav>
 
           <?php print render($page['footer']); ?>
         </footer>
