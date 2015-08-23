09716c56f319625f6bae1ebd9dd0ac8d3ea00d96
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2015-08-23 21:23:23 +0100

    Corrected title on homepage highlight boxes.

--- docroot/sites/all/modules/dev/wsuser/templates/wsuser-member-welcome-block.tpl.php
+++ docroot/sites/all/modules/dev/wsuser/templates/wsuser-member-welcome-block.tpl.php
@@ -7,5 +7,5 @@
  *   - $menu: An array of profile items. Use render() to print them.
  */
 ?>
-<div class='name'><?php print theme('username', array('account' => $account)); ?></div>
+<div class='name'><?php print $profile_link; ?></div>
 <?php print theme('linkbuttons', array('links' => $wsuser_member_welcome_block_menu_links)); ?>
