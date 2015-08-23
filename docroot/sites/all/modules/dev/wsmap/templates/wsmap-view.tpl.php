9e24586bca561ad1b833f5ea1ca7c6ef968c1cd9
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2015-08-23 23:40:36 +0100

    Updated styling and markup for all dashboard elements.

--- docroot/sites/all/modules/dev/wsmap/templates/wsmap-view.tpl.php
+++ docroot/sites/all/modules/dev/wsmap/templates/wsmap-view.tpl.php
@@ -4,14 +4,14 @@
   */
 ?>
 
-<a id="collapse_map" href="#"><?php print t('Collapse Map'); ?></a>
+<a id="collapse_map" class="wsmap-collapse" href="#"><?php print t('Collapse Map'); ?></a>
 <noscript>
       <?php print t("This page isn't going to be much use to you until you turn on javascript in your browser. We apologize."); ?>
 </noscript>
 
-<div id='mapframe' style='width:100%;position:relative;clear:both;'>
-    <div id='mapholder'>
-        <div id='wsmap_map'></div>
+<div id='mapframe' class="wsmap-frame">
+    <div id='mapholder' class="wsmap-wrapper">
+        <div id='wsmap_map' class="wsmap"></div>
     </div>
 </div>
-<div id="wsmap-bottom"><span id="wsmap-load-status"></span><span class="wsmap-credits"><?php print t('Geolocation courtesy of Google and Geonames.org'); ?></span></div>
+<div id="wsmap-bottom" class="wsmap-bottom"><span id="wsmap-load-status" class="wsmap-load-status"></span><span class="wsmap-credits"><?php print t('Geolocation courtesy of Google and Geonames.org'); ?></span></div>
