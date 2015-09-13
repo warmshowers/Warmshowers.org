65d31b6ed523eecec0381d8527c861bc2064e526
Randy Fay <randy@randyfay.com>
2015-09-13 13:31:00 -0600

    Change @localhost to @localhost.dev to have valid email for Drupal

--- resources/devsite_scripts/change_user_emails.sh
+++ resources/devsite_scripts/change_user_emails.sh
@@ -3,5 +3,5 @@
 echo "This script will give dummy emails to all but a few key accounts. It's intended for the database warmshowers. If you want to use it elsewhere, you'll need to change \$db."
 db=warmshowers
 
-echo "update users set mail=concat('user_', uid, '@localhost') where uid not in (1,1165, 12075, 8088, 36456, 18358, 36076);" | mysql $db 
+echo "update users set mail=concat('user_', uid, '@localhost.dev') where uid not in (1,1165, 12075, 8088, 36456, 18358, 36076);" | mysql $db 
 echo "update users set init=mail; " | mysql $db
