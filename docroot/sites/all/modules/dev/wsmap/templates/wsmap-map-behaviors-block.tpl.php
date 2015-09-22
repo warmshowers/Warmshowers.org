0244b2b5cadd1ac11110bcd1bf916301bd0a171c
Jean-François Beaulieu <westjef@gmail.com>
2015-09-21 20:26:26 -0400

    Text styling on the map sidebar options and search blocks. Also some reformatting in the PHP to allow better styling.

--- docroot/sites/all/modules/dev/wsmap/templates/wsmap-map-behaviors-block.tpl.php
+++ docroot/sites/all/modules/dev/wsmap/templates/wsmap-map-behaviors-block.tpl.php
@@ -3,11 +3,9 @@
  * Template for the map behaviors block.
  */
 ?>
-<a href="/searching" target="_blank"><?php print t('Need help searching?'); ?></a><br/>
-<a href="#" id="expand_map"><?php print t("Expand Map"); ?></a>
-<br />
-<input id="hide_markers_checkbox" type="checkbox" />
-  <?php print t('Dim map markers'); ?>
-<br />
-<input id="adv_cyc_checkbox" type="checkbox" />
-<?php print t('Load US Adventure Cycling Routes'); ?>
+<ul>
+	<li><a href="/searching" target="_blank"><?php print t('Need help searching?'); ?></a></li>
+	<li><a href="#" id="expand_map"><?php print t("Expand Map"); ?></a></li>
+	<li><label><input id="hide_markers_checkbox" type="checkbox" /><?php print t('Dim map markers'); ?></label></li>
+	<li><label><input id="adv_cyc_checkbox" type="checkbox" /><?php print t('Load US Adventure Cycling Routes'); ?></label></li>
+</ul>
