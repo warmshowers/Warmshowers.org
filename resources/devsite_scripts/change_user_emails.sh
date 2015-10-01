8c73b0ee8164e67db0be843167fc95872c8a8c63
Randy Fay <randy@randyfay.com>
2015-10-01 06:55:09 -0600

    Update sanitize scripts - should affect Randy only

--- resources/devsite_scripts/change_user_emails.sh
+++ resources/devsite_scripts/change_user_emails.sh
@@ -3,5 +3,5 @@
 db=wsupg
 echo "This script will give dummy emails to all but a few key accounts. It's intended for the database $db. If you want to use it elsewhere, you'll need to change \$db."
 
-echo "update users set mail=concat('user_', uid, '@localhost') where uid not in (1,1165, 12075, 8088, 36456, 18358, 36076);" | mysql $db 
+echo "update users set mail=concat('user_', uid, '@localhost') where uid not in (1,1165, 12075, 8088, 36456, 18358, 36076) AND uid > 0;" | mysql $db
 echo "update users set init=mail; " | mysql $db
--- resources/devsite_scripts/clean_up_languages.sh
+++ resources/devsite_scripts/clean_up_languages.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 webroot=~/workspace/wsupg/docroot
+source_host=warmshowers.org
 devsite_host=wsupg.dev
 # If the English language version of the site has www on it, put "www." here.
 # Otherwise www. will be deleted from the language URLs.
 www_prefix=""
 
-#drush vset language_negotiation 0
-sed "s/warmshowers.org/$devsite_host/g
-s/https/http/g
-s/www./$www_prefix/g" languages.sql  | drush sqlc -r $webroot
+drush sqlc -r $webroot "UPDATE languages SET domain=REPLACE(domain, '$source_host', '$devsite_host');"
+drush sqlc -r $webroot "UPDATE languages SET domain=REPLACE(domain, 'www.$source_host', '$www_prefix$devsite_host') WHERE language='en_working';"
+
 drush cc all
 
-
-"
