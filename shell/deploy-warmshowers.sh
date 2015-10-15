465129d685a370b2c1bb7770f44ec92b6cccf2b4
Randy Fay <randy@randyfay.com>
2015-10-15 16:35:32 -0600

    Ooops, don't want to use deploy-warmshowers.sh as is because it reverts features

--- shell/deploy-warmshowers.sh
+++ shell/deploy-warmshowers.sh
@@ -14,22 +14,18 @@ fi
 
 echo "Deploying to $DEPLOY_TO"
 
-# Ensure files directory is writable.
-sudo chgrp -R $FILES_DIR_GROUP ${DEPLOY_TO}/files
-sudo chmod -R ug+rw $DEPLOY_TO/files
-
-# Remove robots.txt file, we let the robotstxt module handle this.
-rm -f ${DEPLOY_TO}/robots.txt
-
 # Run database updates, compile sass and revert features.
 echo -e "\033[32;40mRunning database updates...\033[0m"
 drush -r ${DEPLOY_TO} updb -y
-echo -e "\033[32;40mRunning registry rebuild...\033[0m"
-drush -r ${DEPLOY_TO} rr -y
+
+// Don't want to accidentally do these thigns
+#echo -e "\033[32;40mRunning registry rebuild...\033[0m"
+#drush -r ${DEPLOY_TO} rr -y
+#echo -e "\033[32;40mReverting features...\033[0m"
+#drush -r ${DEPLOY_TO} fra -y
+
 echo -e "\033[32;40mCompiling sass...\033[0m"
 cd ${DEPLOY_TO}/sites/all/themes/warmshowers_zen && compass compile
-echo -e "\033[32;40mReverting features...\033[0m"
-drush -r ${DEPLOY_TO} fra -y
 
 # Fin.
 echo -e "\033[32;40mClearing all drupal caches...\033[0m"
